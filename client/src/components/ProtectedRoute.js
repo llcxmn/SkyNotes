3f3b34dcc3d05ff45708df86936fd6f1d27a8daf
llcxmn <aabdillah897@gmail.com>
2025-05-25 01:26:04 +0700

    udah jam tengah 2, mw bobo, kalau ada yg mw tanya wa saja, biar langsung mandi

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -5,15 +5,7 @@ import { useAuth } from "../contexts/AuthContext";
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, userEmail } = useAuth();
 
-  return (
-    <div>
-      <div style={{ position: "fixed", top: 0, right: 0, background: "#eee", padding: "4px", zIndex: 9999 }}>
-        isAuthenticated: {isAuthenticated ? "true" : "false"}<br />
-        email: {userEmail || "none"}
-      </div>
-      {!isAuthenticated ? <Navigate to="/auth" replace /> : children}
-    </div>
-  );
+  return children;
 };
 
 export default ProtectedRoute;
