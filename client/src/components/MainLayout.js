ac5cf7106b5341d58b4e71d6aee90b40160ee155
llcxmn <aabdillah897@gmail.com>
2025-05-25 15:08:23 +0700

    liatin dung

--- client/src/components/MainLayout.js
+++ client/src/components/MainLayout.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Sidebar from './Sidebar';
+import Navbar from './Navbar';
 
 const MainLayout = ({ children }) => {
   return (
-    <main className="max-w-xxl mx-auto mt-10 rounded-t-[50px] bg-gradient-to-r from-[#0B3B8A] to-[#0B6FFF] p-8 flex flex-col md:flex-row md:space-x-10 min-h-screen">
-      <Sidebar />
-      <div className="flex-1">
-        {children}
-      </div>
-    </main>
+    <div>
+      <Navbar />
+      <main className="max-w-xxl mx-auto mt-10 rounded-t-[50px] bg-gradient-to-r from-[#0B3B8A] to-[#0B6FFF] p-8 flex flex-col md:flex-row md:space-x-10 min-h-screen">
+        <Sidebar />
+        <div className="flex-1">
+          {children}
+        </div>
+      </main>
+    </div>
   );
 };
 
